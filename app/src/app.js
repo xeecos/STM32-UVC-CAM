6ffce6150342b258bb4848e8ee3a929903955675
虎子哥 <flashindream@gmail.com>
2024-03-12 23:12:36 +0800

    update

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,4 @@
-const { findByIds, WebUSBDevice} = require('usb');
+const { findByIds } = require('usb');
 
 
 const device = findByIds(0x20B1, 0x1DE0);
@@ -16,13 +16,14 @@ const device = findByIds(0x20B1, 0x1DE0);
 async function transfer()
 {
     device.open();
+    console.log(device)
     // device.detatchKernelDriver();
     let interface = device.interfaces[0];
     interface.claim();
 
     // let outEndpoint = endpoints[0];
     // let inEndpoint = endpoints[1];
-    console.log(device.interfaces[0])
+    // console.log(device.interfaces[0])
 }
 transfer();
 // console.log(device)
