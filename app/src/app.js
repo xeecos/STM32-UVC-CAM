3f6f454792e417fa97d3c3150f6bac83a3ca4a8b
xeecos <flashindream@gmail.com>
2024-03-20 18:13:38 +0800

    add bf3003.js

--- app/src/app.js
+++ app/src/app.js
@@ -1,143 +1,31 @@
-const { findByIds,WebUSBDevice ,usb} = require('usb');
 const bmp = require('bmp-js');
 const fs = require("fs")
 const Demosaic = require('demosaic');
- 
+const BF3003 = require("./bf3003");
 
-let data = Buffer.alloc(640*480*4);
-
-let device = findByIds(0x20B1, 0x1DE0);
-async function transfer()
-{
-    let web = new WebUSBDevice(device);
-    web.open();
-    device.interfaces[0].claim();
-    device.interfaces[1].claim();
-    let epIn = device.interfaces[0].endpoints[0];
-    let image = [[],[]];
-    let bufCount = 1;
-    let successCount = 0;
-    let failCount = 0;
-    let totalCount = 0;
-    let line = 0;
-    let cout = 0;
-    let time = Date.now();
-    let imageIdx = 0;
-    let imageWidth = 640;
-    let imageHeight = 480;
-    function encodeData()
+BF3003.setup().then(
+    (res)=>
     {
-        let idx = 1-imageIdx;
-        
-        let rgb = Demosaic.nearestNeighbour({data: Buffer.from(image[idx]), height:imageHeight, width:imageWidth, bayer:Demosaic.Bayer.RGGB});
-        
-        for(let i=0,count=imageWidth*imageHeight;i<count;i++)
+        let data = Buffer.alloc(640*480*4);
+        BF3003.on("data",(res)=>
         {
-            // let g = image[idx][i]&0xff;
-            let n = i<<2;
-            data[n+1] = rgb[i*3];
-            data[n+2] = rgb[i*3+1];
-            data[n+3] = rgb[i*3+2];
-        }
-        console.time("time");
-        let img = bmp.encode({data,width:imageWidth,height:imageHeight});
-        fs.writeFileSync("tmp.bmp",img.data);
-        console.timeEnd("time");
-    }
-    // setInterval(()=>{
-    // },1000);
-    let imageX = (640-imageWidth)/2+6;
-    let imageY = (480-imageHeight)/2;
-    web.transferOut(2,Buffer.from([1,1,imageX>>8,imageX&0xff,imageY>>8,imageY&0xff,imageWidth>>8,imageWidth&0xff,imageHeight>>8,imageHeight&0xff])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,2,0x00,imageWidth==640?0x80:0x00])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,3,0x0,0x10])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,4,0x15,0x12,0x17])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,5,0x20])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,7,16])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,8,1,1,0])).then((res)=>{
-        console.log(res);
-    });
-    web.transferOut(2,Buffer.from([1,0,0x1])).then((res)=>{
-        console.log(res);
-    });
-    // return;
-    epIn.startPoll(1,bufCount*64);
-    // return;
-    epIn.on("data", function(buffer) { 
-        // if(buffer.length==2)
-        // {
-        //     return;
-        // }
-        // cout+=buffer.length;
-        // if(cout>=300*1024)
-        // {
-        //     let t = Date.now()-time;
-        //     console.log(t,":",cout/1024/1024/(t/1000),cout);
-        //     time = Date.now();
-        //     cout = 0;
+            let width = BF3003.width;
+            let height = BF3003.height;
+            if(res.line!=height)return;
+            let rgb = Demosaic.nearestNeighbour({data: Buffer.from(res.data), height, width, bayer:Demosaic.Bayer.RGGB});
+            for(let i=0,count=width*height;i<count;i++)
+            {
+                let n = i<<2;
+                data[n] = 0xff;
+                data[n+1] = rgb[i*3];
+                data[n+2] = rgb[i*3+1];
+                data[n+3] = rgb[i*3+2];
+            }
+            console.time("time");
+            let img = bmp.encode({data,width,height});
+            fs.writeFileSync("tmp.bmp",img.data);
+            console.timeEnd("time");
+        });
 
-        // }
-        // return;
-        if(buffer.length==2)
-        {
-            if(line>10)
-            {
-                let t = Date.now()-time;
-                console.log("encode:",t/1000, line);
-                time = Date.now();
-                imageIdx = 1 - imageIdx;
-
-                if(line>imageHeight-1)
-                {
-                    successCount++;
-                    encodeData();
-                }
-                else
-                {
-                    failCount++;
-                }
-                console.log("percent:",(successCount/(successCount+failCount)*100).toFixed(2),"success:", successCount, " fail:", failCount)
-            }
-            start = true;
-            line = 0;
-            cout = 0;
-            totalCount = 0;
-            // for(let i=0;i<640*480;i++)
-            // {
-            //     image[imageIdx][i] = 0xff;
-            // }
-            return;
-        }
-        if(buffer.length>0)
-        {
-            for(let i=0;i<buffer.length;i++)
-            {
-                image[imageIdx][line*imageWidth+cout+i] = buffer[i];
-                totalCount++;
-            }
-            cout+=buffer.length;
-            if(cout>=imageWidth)
-            {
-                cout = 0;
-                line++;
-            }
-            if(line>=imageHeight)
-            {
-                start = false;
-            }
-        }
+        BF3003.start();
     });
-}
-transfer();
---
+++ app/src/bf3003.js
@@ -0,0 +1,171 @@
+const { findByIds, WebUSBDevice } = require('usb');
+const EventEmitter = require('node:events');
+class BF3003
+{
+    constructor()
+    {
+        this._width = 640;
+        this._height = 480;
+        this._epOut = 2;
+        this._emitter = new EventEmitter();
+    }
+    async setup()
+    {
+        let device = findByIds(0x20B1, 0x1DE0);
+        if(!device)return 1;
+        this._usb = new WebUSBDevice(device);
+        this._usb.open();
+
+        device.interfaces[0].claim();
+        device.interfaces[1].claim();
+        
+        await this.setFrameSize(640,480);
+        await this.setMode(1,1,1);
+        await this.setDummy(this._width==640?0x80:0x00);
+        await this.setExposure(0x10);
+        await this.setFrequency(32);
+
+        let debug = {time:Date.now(),successCount:0,failCount:0,enable:true};
+
+        let line = 0;
+        let imageIdx = 0;
+        let imageBuf = [new Array(640*480),new Array(640*480)];
+        let lineCount = 0;
+
+        let epIn = device.interfaces[0].endpoints[0];
+        epIn.startPoll(1,64);
+        epIn.on("data", (buffer)=>{ 
+            if(buffer.length==2)
+            {
+                if(line>1)
+                {
+                    this.emit("data",{line,data:imageBuf[imageIdx]});
+                    imageIdx = 1 - imageIdx;
+
+                    if(debug.enable)
+                    {
+                        let t = Date.now()-debug.time;
+                        console.log("encode:",t/1000, line);
+                        debug.time = Date.now();
+                        if(line>this._height-1)
+                        {
+                            debug.successCount++;
+                        }
+                        else
+                        {
+                            debug.failCount++;
+                        }
+                        console.log("percent:",(debug.successCount/(debug.successCount+debug.failCount)*100).toFixed(2),"success:", debug.successCount, " fail:", debug.failCount);
+                    }
+                }
+                line = 0;
+                lineCount = 0;
+                return;
+            }
+            if(buffer.length>0)
+            {
+                for(let i=0;i<buffer.length;i++)
+                {
+                    imageBuf[imageIdx][line*this._width+lineCount+i] = buffer[i];
+                }
+                lineCount+=buffer.length;
+                if(lineCount>=this._width)
+                {
+                    lineCount = 0;
+                    line++;
+                }
+            }
+        });
+        return 0;
+    }
+    get width()
+    {
+        return this._width;
+    }
+    get height()
+    {
+        return this._height;
+    }
+    emit(type,data)
+    {
+        this._emitter.emit(type, data);
+    }
+    on(type,callback)
+    {
+        this._emitter.on(type, callback);
+    }
+    setWindow(x,y,w,h)
+    {
+        return new Promise(resolve=>{
+            this._width = w;
+            this._height = h;
+            this._usb.transferOut(this._epOut,Buffer.from([1,1,x>>8,x&0xff,y>>8,y&0xff,w>>8,w&0xff,h>>8,h&0xff])).then((res)=>{
+                resolve(res);
+            });
+        })
+    }
+    setDummy(dummy=0x0)
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut,Buffer.from([1,2,0x00,dummy])).then((res)=>{
+                resolve(res);
+            });
+        })
+    }
+    setFrameSize(w,h)
+    {
+        let x = (640-w)/2+6;
+        let y = (480-h)/2;
+        return this.setWindow(x, y, w, h);
+    }
+    setExposure(exp=0x10)
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut,Buffer.from([1,3,exp>>8,exp&0xff])).then((res)=>{
+                resolve(res);
+            });
+        })
+    }
+    setGain(red=0x15, green=0x12, blue=0x17)
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut,Buffer.from([1,4,red,green,blue])).then((res)=>{
+                resolve(res);
+            });
+        })
+    }
+    setMode(gain=0,colorbalance=0,exposure=0)
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut,Buffer.from([1, 8, gain, colorbalance, exposure])).then((res)=>{
+                resolve(res);
+            })
+        });
+    }
+    setFrequency(freq=16)
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut,Buffer.from([1,7,freq])).then((res)=>{
+                resolve(res);
+            });
+        })
+    }
+    start()
+    {
+        return new Promise(resolve=>{
+            console.log("start")
+            this._usb.transferOut(this._epOut, Buffer.from([1,0,0x1])).then((res)=>{
+                resolve(res);
+            });
+        });
+    }
+    stop()
+    {
+        return new Promise(resolve=>{
+            this._usb.transferOut(this._epOut, Buffer.from([1,0,0x0])).then((res)=>{
+                resolve(res);
+            });
+        });
+    }
+}
+module.exports = new BF3003();
